18cf650c9e5ddaeadb6b607c87b76551a7196d97
fhasnur <fandi.hasnur10@gmail.com>
2024-04-24 01:06:56 +0700

    feat: get user API

--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -1,3 +1,4 @@
+import { User } from "@prisma/client";
 import { prismaClient } from "../application/database";
 import { ResponseError } from "../error/response-error";
 import { CreateUserRequest, LoginUserRequest, UserResponse, toUserResponse } from "../model/user-model";
@@ -58,5 +59,9 @@ export class UserService {
     const response = toUserResponse(user);
     response.token = user.token!;
     return response;
+  }
+
+  static async get(user: User): Promise<UserResponse> {
+    return toUserResponse(user);
   }
 }
