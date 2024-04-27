fbd39ca93c3b4e0b83ecfc5be7583a3f1ac0c72c
fhasnur <fandi.hasnur10@gmail.com>
2024-04-27 22:41:37 +0700

    feat: get contact API

--- src/service/contact-service.ts
+++ src/service/contact-service.ts
@@ -3,6 +3,7 @@ import { ContactResponse, CreateContactRequest, toContactResponse } from "../mod
 import { ContactValidation } from "../validation/contact-validation";
 import { Validation } from "../validation/validation";
 import { prismaClient } from "../application/database";
+import { ResponseError } from "../error/response-error";
 
 export class ContactService {
 
@@ -17,6 +18,21 @@ export class ContactService {
     const contact = await prismaClient.contact.create({
       data: record
     });
+
+    return toContactResponse(contact);
+  }
+
+  static async get(user: User, id: number): Promise<ContactResponse> {
+    const contact = await prismaClient.contact.findUnique({
+      where: {
+        id: id,
+        username: user.username
+      }
+    });
+
+    if (!contact) {
+      throw new ResponseError(404, "Contact is not found");
+    }
 
     return toContactResponse(contact);
   }
