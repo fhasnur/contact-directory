820c3d329ab6419cc33f950b5ce7684f99e5521e
fhasnur <fandi.hasnur10@gmail.com>
2024-05-03 21:42:00 +0700

    feat: remove address API

--- src/service/address-service.ts
+++ src/service/address-service.ts
@@ -1,5 +1,5 @@
 import { Address, User } from "@prisma/client";
-import { AddressResponse, CreateAddressRequest, GetAddressRequest, UpdateAddressRequest, toAddressResponse } from "../model/address-model";
+import { AddressResponse, CreateAddressRequest, GetAddressRequest, RemoveAddressRequest, UpdateAddressRequest, toAddressResponse } from "../model/address-model";
 import { Validation } from "../validation/validation";
 import { AddressValidation } from "../validation/address-validation";
 import { ContactService } from "./contact-service";
@@ -51,6 +51,20 @@ export class AddressService {
         contact_id: updateRequest.contact_id
       },
       data: updateRequest
+    });
+
+    return toAddressResponse(address);
+  }
+
+  static async remove(user: User, request: RemoveAddressRequest): Promise<AddressResponse> {
+    const removeRequest = Validation.validate(AddressValidation.GET, request);
+    await ContactService.checkContactMustExists(user.username, request.contact_id);
+    await this.checkAddressMustExists(removeRequest.contact_id, removeRequest.id);
+
+    const address = await prismaClient.address.delete({
+      where: {
+        id: removeRequest.id
+      }
     });
 
     return toAddressResponse(address);
